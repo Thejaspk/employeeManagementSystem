12b69336d56e393ad7a77519f5e0d2ceb1adc1b3
tejas <tejas@kranadmin>
2025-06-04 16:56:25 +0530

    retail_cloud employee management system

---
+++ employee_management_system/src/test/java/com/retail_cloud/employee_management_system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail_cloud.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
